0846d5bb778a17f81ccc46ec3f0df37f0cee741e
aadesolaferanmi <aadesolaferanmi@gmail.com>
2024-10-18 18:06:50 +0100

    Add files via upload

---
+++ david.py
@@ -0,0 +1,17 @@
+a=33
+b= 33
+if b>a:
+    print("b is greater than a")
+elif a==b:
+   print("a and b are equal")
+  # ans=a and b are equal
+
+   a=200
+   b=33
+   if b>a:
+    print("b is greater than a")
+elif a==b:
+   print("a and b are equal")
+else:
+   print("if a is greater than b")
+   ans=a and b are equal
